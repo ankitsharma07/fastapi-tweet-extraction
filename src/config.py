1d5e7eddef85e256e2c93d1f766efcf3f1542d23
Ankit Sharma <nez.dev@icloud.com>
2022-10-13 10:22:07 +0530

    paths in src

--- src/config.py
+++ src/config.py
@@ -4,10 +4,10 @@ MAX_LEN = 128
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 16
 EPOCHS = 10
-BERT_PATH = "../input/bert-base-uncased"
-MODEL_PATH = "../models/model.bin"
-TRAINING_FILE = "../input/train.csv"
-TEST_FILE = "../input/test.csv"
+BERT_PATH = "../app/input/bert-base-uncased"
+MODEL_PATH = "../app/models/model.bin"
+TRAINING_FILE = "../app/input/train.csv"
+TEST_FILE = "../app/input/test.csv"
 
 print('bert', f"{BERT_PATH}/vocab.txt")
 TOKENIZER = tokenizers.BertWordPieceTokenizer(
